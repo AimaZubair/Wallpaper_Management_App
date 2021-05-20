6d7ef29cc51d157a53841edbc7b333298b7c55e6
HafsaFaryad <hafsafaryad3@gmail.com>
2021-05-20 21:06:45 +0500

    wallpaper management app
    project created, splash screen added

---
+++ walpaper_app/walpaper_app/lib/splashscreen.dart
@@ -0,0 +1,117 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flare_flutter/flare_actor.dart';
+
+class splashscreen extends StatefulWidget {
+  @override
+  _splashscreenState createState() => _splashscreenState();
+}
+
+class _splashscreenState extends State<splashscreen> {
+  @override
+  void initState() {
+    super.initState();
+  //  Timer(Duration(seconds: 35), () {
+     // Navigator.of(context).pushReplacement(MaterialPageRoute(
+     //   builder: (context) => homepage(),
+  //    ));
+  //  });
+  }
+
+  // added test yourself
+  // and made the text to align at center
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        centerTitle: true,
+        title: Text(
+          "Wallpaper Management App",
+          style: TextStyle(
+            fontFamily: "serif",
+          ),
+        ),
+      ),
+
+
+      body: Center(
+
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: ExactAssetImage("images/2.jpg"),
+                  fit: BoxFit.cover
+              ),
+            ),
+            child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+            SizedBox(
+              height: 20.0,
+            ),
+        SizedBox(
+          child: ColorizeAnimatedTextKit(
+              onTap: () {
+                print("Tap Event");
+              },
+              text: [
+                "Aima Zubair\n(FA17-BCS-009)",
+                "Faizan Ali\n(FA17-BCS-016)",
+                "Filza Mukhtar\n(FA17-BCS-021)",
+                "Hafsa Faryad\n(FA17-BCS-025)",
+                "Jawaria Bashir\n(FA17-BCS-031)",
+                "Maria Nadeem\n(FA17-BCS-039)",
+              ],
+
+
+              textStyle: TextStyle(
+                  fontSize: 30.0,
+
+              ),
+              colors: [
+                Colors.red,
+                Colors.blueGrey,
+                Colors.yellow,
+                Colors.greenAccent,
+              ],
+              textAlign: TextAlign.center,
+
+            ),
+
+        ),
+            SizedBox(
+              height: 10.0,
+              child: Divider(
+
+              ),
+            ),
+
+
+            CircleAvatar(
+              backgroundColor: Colors.grey,
+              radius: 75,
+              backgroundImage:
+                  AssetImage('images/load.gif'),
+            ),
+
+
+            Center(
+              child: Container(
+                width: 200,
+                height: 200,
+                child: FlareActor(
+                  "assets/flareintro.flr",
+                  animation: "Build and Fade Out",
+
+                ),
+              ),
+            )
+        ],
+      ),
+          )),
+    );
+  }
+
+}
