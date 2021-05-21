b6d9b46deb2555b9f29f20f6cac3f12a702ee2cd
Maria Nadeem <79501208+Maria039@users.noreply.github.com>
2021-05-21 19:44:24 +0500

    Update main.dart

--- walpaper_app/walpaper_app/lib/main.dart
+++ walpaper_app/walpaper_app/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Calculator",
+      title: "Wallpaper_App",
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
